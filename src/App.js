349af30442fae588034d4a6d066a10600e7002e9
Abiodun Abolarinwa <47521066+biocell@users.noreply.github.com>
2022-11-05 12:12:50 +0100

    fix: Renamed the Home component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import Homepage from './Homepage';
+import Homepage from './Home';
 import Contact from './contact';
 
 function App(){
